2cb8058377968e7ef42226f097ff84a4c5cee625
Mohit Kishore <gkmohit@gmail.com>
2015-11-08 01:22:01 -0500

    Completed version of NewAppointment.java

--- app/src/main/java/com/unknown/gkmohit/rbcassist/NewAppointment.java
+++ app/src/main/java/com/unknown/gkmohit/rbcassist/NewAppointment.java
@@ -3,7 +3,9 @@ package com.unknown.gkmohit.rbcassist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -13,6 +15,8 @@ import butterknife.ButterKnife;
 import io.heckathon.rbcassist.domain.AccountType;
 import io.heckathon.rbcassist.domain.Customer;
 import io.heckathon.rbcassist.domain.ServiceType;
+import io.heckathon.rbcassist.domain.UrgencyType;
+import io.heckathon.rbcassist.handlers.impl.AppointmentHandlerImpl;
 import io.heckathon.rbcassist.handlers.impl.StaticDataHanlder;
 
 public class NewAppointment extends AppCompatActivity {
@@ -23,7 +27,15 @@ public class NewAppointment extends AppCompatActivity {
 
     @Bind(R.id.typeSpinner)
     Spinner mTypeSpinner;
+
+    @Bind(R.id.urgencySpinner)
+    Spinner mUrgencySpinner;
+
+    @Bind(R.id.nextButton)
+    Button mNextButton;
+
     Customer mCustomer = new Customer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +46,7 @@ public class NewAppointment extends AppCompatActivity {
         mCustomer = (Customer) intent.getSerializableExtra("customer");
 
         //Account type
+        //
         ArrayList<String> accountTypeArrayList = new ArrayList<String>();
 
         for ( AccountType account : StaticDataHanlder.getAccountList()){
@@ -45,7 +58,8 @@ public class NewAppointment extends AppCompatActivity {
         mCategorySpinner.setAdapter(accountAdapter);
 
         //Service type
-        ArrayList<String> serviceArrayList = new ArrayList<String>();
+        //
+        final ArrayList<String> serviceArrayList = new ArrayList<String>();
 
         for ( ServiceType service : StaticDataHanlder.getServiceList()){
             serviceArrayList.add(service.getServiceType());
@@ -55,9 +69,35 @@ public class NewAppointment extends AppCompatActivity {
         ArrayAdapter<String> serviceAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, serviceStringArray);
         mTypeSpinner.setAdapter(serviceAdapter);
 
-        //Agent names
-        //ArrayList<String>
+        //Urgency level
+        //
+        ArrayList<String> urgencyArrayList = new ArrayList<String>();
+
+        for ( UrgencyType urgencyType : StaticDataHanlder.getUrgencyTypeList()){
+            urgencyArrayList.add(urgencyType.getUrgencyType());
+        }
+        String[] urgencyStringArray = new String[urgencyArrayList.size()];
+        urgencyArrayList.toArray(urgencyStringArray);
+        ArrayAdapter<String> urgencyAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, urgencyStringArray);
+        mUrgencySpinner.setAdapter(urgencyAdapter);
 
 
+        //String customerId, String agentId, String serviceId, String location, Date date, int waittime
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String customerId = new String(mCustomer.getCustomerId());
+                String serviceId = new String(StaticDataHanlder.getServiceId(mTypeSpinner.getSelectedItem().toString()));
+                int waitTime = AppointmentHandlerImpl.getWaitTime();
+
+                Intent myIntent = new Intent(NewAppointment.this, PastAppointments.class);
+                //Optional parameters
+                myIntent.putExtra("customer", mCustomer);
+                myIntent.putExtra("serviceId", serviceArrayList);
+                myIntent.putExtra("waitTime", waitTime);
+                NewAppointment.this.startActivity(myIntent);
+
+            }
+        });
     }
 }

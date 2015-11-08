f0658c92865651b48e34ecff675fd42340a3e9dd
Mohit Kishore <gkmohit@gmail.com>
2015-11-07 23:15:09 -0500

    Working on new appointment

--- app/src/main/java/com/unknown/gkmohit/rbcassist/NewAppointment.java
+++ app/src/main/java/com/unknown/gkmohit/rbcassist/NewAppointment.java
@@ -1,37 +1,63 @@
 package com.unknown.gkmohit.rbcassist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.heckathon.rbcassist.domain.AccountType;
+import io.heckathon.rbcassist.domain.Customer;
+import io.heckathon.rbcassist.domain.ServiceType;
+import io.heckathon.rbcassist.handlers.impl.StaticDataHanlder;
 
 public class NewAppointment extends AppCompatActivity {
 
+    //Member variables
+    @Bind(R.id.categorySpinner)
+    Spinner mCategorySpinner;
+
+    @Bind(R.id.typeSpinner)
+    Spinner mTypeSpinner;
+    Customer mCustomer = new Customer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_appointment);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_appointment, menu);
-        return true;
-    }
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        mCustomer = (Customer) intent.getSerializableExtra("customer");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //Account type
+        ArrayList<String> accountTypeArrayList = new ArrayList<String>();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for ( AccountType account : StaticDataHanlder.getAccountList()){
+            accountTypeArrayList.add(account.getAccountType());
         }
+        String[] accountTypeStringArray = new String[accountTypeArrayList.size()];
+        accountTypeArrayList.toArray(accountTypeStringArray);
+        ArrayAdapter<String> accountAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, accountTypeStringArray);
+        mCategorySpinner.setAdapter(accountAdapter);
 
-        return super.onOptionsItemSelected(item);
+        //Service type
+        ArrayList<String> serviceArrayList = new ArrayList<String>();
+
+        for ( ServiceType service : StaticDataHanlder.getServiceList()){
+            serviceArrayList.add(service.getServiceType());
+        }
+        String[] serviceStringArray = new String[serviceArrayList.size()];
+        serviceArrayList.toArray(serviceStringArray);
+        ArrayAdapter<String> serviceAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, serviceStringArray);
+        mTypeSpinner.setAdapter(serviceAdapter);
+
+        //Agent names
+        //ArrayList<String>
+
+
     }
 }

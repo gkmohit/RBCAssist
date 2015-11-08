1d4d20178823c945fa8b45d97a431d7f2f1f2328
Mohit Kishore <gkmohit@gmail.com>
2015-11-08 10:09:42 -0500

    Updated CallLater page for Nileesh

--- app/src/main/java/com/unknown/gkmohit/rbcassist/CallLater.java
+++ app/src/main/java/com/unknown/gkmohit/rbcassist/CallLater.java
@@ -1,10 +1,8 @@
 package com.unknown.gkmohit.rbcassist;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,12 +12,9 @@ import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import io.heckathon.rbcassist.domain.AccountType;
 import io.heckathon.rbcassist.domain.Agent;
 import io.heckathon.rbcassist.domain.Appointment;
 import io.heckathon.rbcassist.domain.Customer;
-import io.heckathon.rbcassist.domain.ServiceType;
-import io.heckathon.rbcassist.domain.UrgencyType;
 import io.heckathon.rbcassist.handlers.impl.AppointmentHandlerImpl;
 import io.heckathon.rbcassist.handlers.impl.StaticDataHanlder;
 
@@ -85,40 +80,21 @@ public class CallLater extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //String customerId = new String(mCustomer.getCustomerId());
-                    String serviceId = new String(StaticDataHanlder.getServiceId(mDateSpinner.getSelectedItem().toString()));
-                    int waitTime = AppointmentHandlerImpl.getWaitTime();
-                    String category = new String(StaticDataHanlder.getAccountId(mTimeSpinner.getSelectedItem().toString()));
-                    Intent myIntent = new Intent(CallLater.this, ConfirmationActivity.class);
-                    //Optional parameters
-                    myIntent.putExtra("customer", mCustomer);
-                    myIntent.putExtra("serviceId", serviceId);
-                    myIntent.putExtra("waitTime", waitTime);
-                    myIntent.putExtra("category", category);
-                    CallLater.this.startActivity(myIntent);
+                String serviceId = new String(StaticDataHanlder.getServiceId(mDateSpinner.getSelectedItem().toString()));
+                int waitTime = AppointmentHandlerImpl.getWaitTime();
+                String category = new String(StaticDataHanlder.getAccountId(mTimeSpinner.getSelectedItem().toString()));
+                Intent myIntent = new Intent(CallLater.this, CallLaterConfirmationActivity.class);
+                //TODO Create appoint object to pass to next intent
+                Appointment appointment = new Appointment();
+                //Optional parameters
+                myIntent.putExtra("customer", mCustomer);
+                myIntent.putExtra("apointment", appointment);
+//                myIntent.putExtra("serviceId", serviceId);
+//                myIntent.putExtra("waitTime", waitTime);
+//                myIntent.putExtra("category", category);
+                CallLater.this.startActivity(myIntent);
             }
         });
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_call_later, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }

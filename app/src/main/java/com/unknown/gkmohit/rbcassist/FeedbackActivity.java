9acf9d1ab20329ee27764fd5b9436ed58d3651ad
Mohit Kishore <gkmohit@gmail.com>
2015-11-08 09:46:47 -0500

    Finished feedback activity.

---
+++ app/src/main/java/com/unknown/gkmohit/rbcassist/FeedbackActivity.java
@@ -0,0 +1,74 @@
+package com.unknown.gkmohit.rbcassist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.heckathon.rbcassist.domain.Appointment;
+import io.heckathon.rbcassist.domain.Customer;
+import io.heckathon.rbcassist.handlers.impl.StaticDataHanlder;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+
+    //Member variables
+    @Bind(R.id.agentNameTV)
+    TextView mAgentNameTV;
+
+    @Bind(R.id.appointmentDateTV)
+    TextView mAppointmentDateTV;
+
+    @Bind(R.id.serviceTypeTV)
+    TextView mServiceTypeTV;
+
+    @Bind(R.id.ratingBar)
+    RatingBar mRatingBar;
+
+    @Bind(R.id.commentET)
+    EditText mCommentET;
+
+    @Bind(R.id.submitButton)
+    Button mSubmitButton;
+
+    Customer mCustomer = new Customer();
+    Appointment mAppointment = new Appointment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        mCustomer = (Customer) intent.getSerializableExtra("customer");
+        mAppointment = (Appointment) intent.getSerializableExtra("appointment");
+
+        mAgentNameTV.setText(mAppointment.getAgentName());
+        mAppointmentDateTV.setText(mAppointment.getStrDate());
+        mServiceTypeTV.setText(StaticDataHanlder.getServiceType(mAppointment.getServiceTypeId()));
+        mRatingBar.setRating(5);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(FeedbackActivity.this, WelcomeActivity.class);
+                //Optional parameters
+                myIntent.putExtra("customer", mCustomer);
+                finish();
+                Toast.makeText(view.getContext(), "Your feed back has been submitted. Thank you", Toast.LENGTH_LONG).show();
+                FeedbackActivity.this.startActivity(myIntent);
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/unknown/gkmohit/rbcassist/PastAppointments.java
+++ app/src/main/java/com/unknown/gkmohit/rbcassist/PastAppointments.java
@@ -1,12 +1,11 @@
 package com.unknown.gkmohit.rbcassist;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -42,13 +41,22 @@ public class PastAppointments extends AppCompatActivity {
         mCurrentAppointmentsTable.setColumnStretchable(0, true);
         mCurrentAppointmentsTable.setColumnStretchable(1, true);
         mCurrentAppointmentsTable.setColumnStretchable(2, true);
-        for(Appointment appointment : mCustomer.getCompletedAppointments()){
+        for(final Appointment appointment : mCustomer.getCompletedAppointments()){
 
             TextView dateTV = new TextView(this);
             TextView agentTV = new TextView(this);
             TextView typeTV = new TextView(this);
             TextView statusTV = new TextView(this);
             TableRow row = new TableRow(this);
+            //Button viewButton = new Button(this);
+
+            //Setting button view
+//            viewButton.setText("View");
+//            viewButton.setBackgroundColor(getResources().getColor(R.color.button_background_color));
+//            viewButton.setTextColor(getResources().getColor(R.color.button_text_color));
+//            viewButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//            viewButton.setWidth();
+
             //Date text view
             dateTV.setText(appointment.getStrDate());
             dateTV.setTextSize(15);
@@ -64,16 +72,35 @@ public class PastAppointments extends AppCompatActivity {
             typeTV.setGravity(Gravity.CENTER);
 
             //Status text view
+            if(appointment.getStatus().toUpperCase().equals("COMPLETED")) {
+                statusTV.setBackgroundColor(Color.parseColor("#70C14B"));
+            }else if(appointment.getStatus().toUpperCase().equals("PROCESSING")){
+                statusTV.setBackgroundColor(Color.parseColor("#FA7900"));
+            }
+            else{
+                statusTV.setBackgroundColor(Color.parseColor("#F70A21"));
+            }
             statusTV.setText(appointment.getStatus());
             statusTV.setTextSize(15);
             statusTV.setGravity(Gravity.CENTER);
-
             //Adding text view to the table
 
             row.addView(dateTV);
             row.addView(typeTV);
             row.addView(statusTV);
+            row.setPadding(0, 10, 0, 0);
+//            row.addView(viewButton);
             mCurrentAppointmentsTable.addView(row);
+            statusTV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent myIntent = new Intent(PastAppointments.this, FeedbackActivity.class);
+                    //Optional parameters
+                    myIntent.putExtra("customer", mCustomer);
+                    myIntent.putExtra("appointment",appointment);
+                    PastAppointments.this.startActivity(myIntent);
+                }
+            });
         }
 
     }
